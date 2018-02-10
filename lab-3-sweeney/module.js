00761d347caac4f1f3e94951676e58b73f331039
Caitlin <caitlinsweeneylmt@gmail.com>
2018-02-10 15:11:55 -0800

    still working on module.js file

--- lab-3-sweeney/module.js
+++ lab-3-sweeney/module.js
@@ -1,26 +1,42 @@
 'use strict';
 
 
-Readthreefiles([], readerCb);
+const fs = require('fs');
+const inputFile = [
+  '../assets/file1.txt',
+  '../assets/file2.txt',
+  '../assets/file3.txt'
+];
+const outputFile = [];
 
-
-function readerCb(arr,results){ 
-
-  fs.readFile('../assets/file1.txt', yesOrno(err,results));
-  fs.readFile('../assets/file2.txt', yesOrno(err,results));
-  fs.readFile('../assets/file3.txt', yesOrno(err,results));
-
-};
-
-function yesOrno(err,results){
-
+function readCb(err,results){ 
   if(err){
-    console.log("there was an error reading text file")
+    console.log('there was an error reading text file');
   };
 
-  if(results){
-    readerCb(null,results);
-  }
+  let file = results.toString();
 
+  return file;
+  console.log('file:',file);
 };
 
+// outputFile[0] = fs.readFile(inputFile, readCb);
+// outputFile[1] = fs.readFile(inputFile, readCb);
+// outputFile[2] = fs.readFile(inputFile, readCb);
+
+inputFile.forEach(function(){
+  let output = fs.readFile(inputFile, readCb);
+  console.log('output:', output);
+  outputFile.push(output);
+});
+
+
+
+
+module.exports.Readthreefiles;
+
+
+// fs.readFile(inputFile[1], readCb(err, results));
+
+
+// fs.readFile(inputFile[2], readCb(err, results));
