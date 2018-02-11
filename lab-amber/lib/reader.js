a13c17c59c250ba800a552d60343dc9c383d3a2d
amgranad <ambergkim@gmail.com>
2018-02-10 16:45:17 -0800

    more tests for invalid inputs

--- lab-amber/lib/reader.js
+++ lab-amber/lib/reader.js
@@ -7,32 +7,41 @@ let Reader = {};
 Reader.newFileArray = [];
 Reader.readCount = 0;
 
-Reader.errorCallback = (err) => {
+Reader.errorCallback = (err, sendErrorCallBack) => {
   console.log('Error callback error', err);
+  return err;
 }
 
-Reader.read = (filePath, callBack, index, sendFileCB) => {
+Reader.read = (filePath, callBack, index, sendFileCB, err) => {
   fs.readFile(filePath, (err, data) => {
-    if (err) Reader.errorCallback(err);
+    if (err) Reader.errorCallback(sendFileCB(undefined));
     Reader.readCallBack(null, data, index, sendFileCB);
   });
 } 
 
 Reader.readCallBack = (err, results, index, sendFileCB) => {
-  if (err) errorCallback(err);
-  let readBook = results.toString();
-  Reader.newFileArray[index] = readBook;
-  if (Reader.readCount === 3) {
-    sendFileCB(false, Reader.newFileArray);
+  if (results === undefined) {
+    Reader.errorCallback(undefined);
+  } else {
+    let readBook = results.toString();
+    Reader.newFileArray[index] = readBook;
+    if (Reader.readCount === 3) {
+      sendFileCB(false, Reader.newFileArray);
+    }
   }
+  
 }
 
 Reader.readAll = (array, sendFileCB) => {
   if (array === null) {
-    Reader.errorCallback(422);
+    Reader.errorCallback(sendFileCB(null));
   } else {
     for (let i = 0; i < array.length; i++) {
       let currentPath = array[i];
+      // console.log(currentPath, 'current path');
+      // if (currentPath === undefined) {
+      //   sendFileCB(404);
+      // }
       Reader.readCount++;
       Reader.read(currentPath, Reader.readCallBack, i, sendFileCB);
     }
--- lab-amber/test/reader.test.js
+++ lab-amber/test/reader.test.js
@@ -2,6 +2,7 @@
 
 const Reader = require('../lib/reader.js');
 
+// PASSING
 describe('Test with for an empty array', () => {
   let expected = [];
   it ('An empty array should be returned', () => {
@@ -13,9 +14,22 @@ describe('Test with for an empty array', () => {
   })
 });
 
+// PASSING
+describe('Test with for an array with just one item', () => {
+  let expected = ['hello world!'];
+  it ('An empty array should be returned', () => {
+    let filePathArray = [`./assets/hello.txt`];
+    let result = Reader.readAll(filePathArray, (err, results) => {
+      expect(results).toBe(expected);
+      resolve();
+    })
+  })
+});
+
+// PASSING
 describe('Test with for null instead of an array', () => {
   let expected = undefined;
-  it ('An error should be returned', () => {
+  it ('An error should be returned', (resolve, reject) => {
     let filePathArray = null;
     let result = Reader.readAll(filePathArray, (err, results) => {
       expect(results).toBe(expected);
@@ -23,6 +37,19 @@ describe('Test with for null instead of an array', () => {
     })
   })
 });
+
+// PASSING
+describe('Test with an array that has an invalid path', () => {
+  let expected = undefined;
+  it('this right order: pride and prejudice, hellow world, peace negotiations', (resolve, reject) => {
+    let filePathArray = [`./assets/pride-prejudice.txt`, `./assets/hello2.txt`, `./assets/peace-negotiations.txt` ];
+    let result = Reader.readAll(filePathArray, (err, results) => {
+      expect(results).toBe(expected);
+      resolve();
+    });
+  })
+});
+
 
 // HALFWAY WORKS
 describe('Test with a correct array', () => {
