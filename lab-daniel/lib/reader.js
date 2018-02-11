ee50d4c9eb47811fec0b926dbb51b697ad551d77
daniel <dpillay03@gmail.com>
2018-02-10 17:39:49 -0800

    all my files are logging onto my terminal

--- lab-daniel/lib/reader.js
+++ lab-daniel/lib/reader.js
@@ -4,13 +4,37 @@ const fs = require('fs');
 const paths = (['../assets/dracula.txt', '../assets/peterpan.txt', '../assets/siddhartha.txt']);
 
 
-fs.readFile('../assets/dracula.txt', (err, data) => {
+fs.readFile(paths[0], (err, data) => {
     if (err) {
-      if (err.code === 'EEXIST') {
+      if (err.code === 'EXIST') {
         console.error('myfile already exists');
         return;
       }
-  
+      throw err;
+    }
+    let str = data.toString()
+    console.log(str);
+  });
+
+fs.readFile(paths[1], (err, data) => {
+    if (err) {
+      if (err.code === 'EXIST') {
+        console.error('myfile already exists');
+        return;
+      }
+      throw err;
+    }
+    let str = data.toString()
+    console.log(str);
+  });
+
+
+fs.readFile(paths[2], (err, data) => {
+    if (err) {
+      if (err.code === 'EXIST') {
+        console.error('myfile already exists');
+        return;
+      }
       throw err;
     }
     let str = data.toString()
