99c1ca58040580a8bb4d4761294fce902f9312a8
daniel <dpillay03@gmail.com>
2018-02-10 17:02:53 -0800

    my first text file (draculat.txt) is logging on my terminal

--- lab-daniel/lib/reader.js
+++ lab-daniel/lib/reader.js
@@ -1,4 +1,18 @@
 'use strict';
 
 const fs = require('fs');
+const paths = (['../assets/dracula.txt', '../assets/peterpan.txt', '../assets/siddhartha.txt']);
 
+
+fs.readFile('../assets/dracula.txt', (err, data) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        console.error('myfile already exists');
+        return;
+      }
+  
+      throw err;
+    }
+    let str = data.toString()
+    console.log(str);
+  });
