7c9d6b7f889a108b5652c97b19433015c8ad5a6e
daniel <dpillay03@gmail.com>
2018-02-18 19:31:14 -0800

    created a test to see if typeof array is strings. it passed!

--- lab-daniel/lib/reader.js
+++ lab-daniel/lib/reader.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const fs = require('fs');
+const reader = module.exports = {};
 const paths = (['../assets/dracula.txt', '../assets/peterpan.txt', '../assets/siddhartha.txt']);
 
-
-fs.readFile(paths[0], (err, data) => {
+reader.readFile = (paths, callback) => {
+  fs.readFile(paths[0], (err, data) => {
     let files = [];
     let str = data.toString();
     if (err) {
@@ -17,31 +18,32 @@ fs.readFile(paths[0], (err, data) => {
     files.push(str);
     console.log(str);
 
-fs.readFile(paths[1], (err, data) => {
+  fs.readFile(paths[1], (err, data) => {
+    let files = [];
+    let str = data.toString();
     if (err) {
-    if (err.code === 'EXIST') {
-    console.error('myfile already exists');
-    return;
-    }
-    throw err;
+        if (err.code === 'EXIST') {
+        console.error('myfile already exists');
+        return;
+      }
+      throw err;
     }
     files.push(str);
     console.log(str);
 
-fs.readFile(paths[2], (err, data) => {
+  fs.readFile(paths[2], (err, data) => {
+    let files = [];
+    let str = data.toString();
     if (err) {
-    if (err.code === 'EXIST') {
-    console.error('myfile already exists');
-    return;
-    }
-    throw err;
+        if (err.code === 'EXIST') {
+        console.error('myfile already exists');
+        return;
+      }
+      throw err;
     }
     files.push(str);
     console.log(str);
     });
-  });
-});
-
-
-module.exports = {};
-module.exports.fs.readfile = fs.readfile;
+   });
+ });
+}
