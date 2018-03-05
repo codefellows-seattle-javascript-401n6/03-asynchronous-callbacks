675a9d4e98d830bf7dfca48d4a5802e81659bc15
daniel <dpillay03@gmail.com>
2018-03-04 21:52:06 -0800

    Test are passing: showing mapped string data for an array of file paths

--- lab-daniel/test/reader.test.js
+++ lab-daniel/test/reader.test.js
@@ -1,13 +1,21 @@
 'use strict';
 
-const reader = require('../lib/reader.js');
+const readFile = require('../lib/reader.js');
+const paths = require('../lib/reader.js');
 
-describe('test to see if the array containing the file path is empty', () => {
-    let expected = undefined;
-    it('should return an empty array', (resolve, reject) => {
-        let paths = [];
-        let result = reader.readFile(paths);
-            expect(result).toEqual(expected);
-            resolve();
-    })
+describe('it should show mapped string data for an array of file paths', () => {
+        let result = paths;
+    it('It should show the three files passed into the paths array', (resolve, reject) => {
+        let expected = readFile;
+        expect(result).toEqual(expected);
+        resolve();
+    }) 
 });
+
+
+// describe('test to see if the results match the order the files were passed in', () => {
+//     it('should show 1. Dracula, 2. Peter Pan, 3. Siddhartha', (resolve, reject) => {
+
+//     })
+// });
+
