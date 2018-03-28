7df5ed076f46f2f2896ecbcda0044457cb0b3c48
Caitlin <caitlinsweeneylmt@gmail.com>
2018-03-28 16:19:43 -0700

    having issues linking file to test

--- lab-3-sweeney/lib/read.js
+++ lab-3-sweeney/lib/read.js
@@ -1,73 +1,56 @@
 'use strict';
 
-// Readthreefiles(['../assets/file1.txt',
-// '../assets/file2.txt',
-// '../assets/file3.txt'],readCb);
-const fs = require('fs');
-const each = require('async-each');
-const inputFile = [
-  '../assets/file1.txt',
-  '../assets/file2.txt',
-  '../assets/file3.txt'
-];
-const outputFile = [];
+const Readthreefiles = (function(){
+  const fs = require('fs');
 
-function readCb(err,results){ 
-  if(err){
-    console.log('there was an error reading text file');
+  function readCb(err,results){ 
+    if(err){
+      console.log('there was an error reading text file');
+    };
+
+    let file = results.toString();
+
+    outputFile.push(file);
+    console.log('file:',file);
   };
+  const inputFiles = [
+    '../assets/file1.txt',
+    '../assets/file2.txt',
+    '../assets/file3.txt'
+  ];
 
-  let file = results.toString();
+  const outputFiles = [];
 
-  outputFile.push(file);
-  console.log('file:',file);
-};
+  function completedAll() {
+    console.log('completed all');
+    return outputFiles;
+  }
 
-// each(inputFile, fs.readFile, readCb);
+  let delay = 10000;
+  const arrayOfPromises = Object.keys(inputFiles).map(file => (
+    new Promise((resolve, reject) => {
+      const content = fs.readFile(inputFiles[index]);
+      console.log('request', content);
+      setTimeout((err, resp, body) => {
+        if (err) {
+          reject(err);
+        }
 
-console.log('output array', outputFile);
-// fs.readFile(inputFile[0], readCb);
-// fs.readFile(inputFile[1], readCb);
-// fs.readFile(inputFile[2], readCb);
+        // Some code for which I use object values
+        resolve(file);
+      }, (delay /= 2));
+    }))
+    .then(readCb));
+
+  Promise.all(arrayOfPromises)
+    .then(completedAll)
+    .catch(console.error);
 
 
+  console.log('output array', outputFile);
+}());
 
 module.exports.Readthreefiles;
 
 
-// fs.readFile(inputFile[1], readCb(err, results));
 
-
-// fs.readFile(inputFile[2], readCb(err, results));
-const inputFile = [
-  '../assets/file1.txt',
-  '../assets/file2.txt',
-  '../assets/file3.txt'
-];
-function completedAll() {
-  console.log('completed all');
-}
-
-function fileRead(file) {
-  console.log('completed', courseName);
-}
-
-let delay = 10000;
-const arrayOfPromises = Object.keys(inputFiles).map(file => (
-  new Promise((resolve, reject) => {
-    const url = `...${inputFiles[file]}`;
-    console.log('request', url);
-    setTimeout((err, resp, body) => {
-      if (err) {
-        reject(err);
-      }
-
-      // Some code for which I use object values
-      resolve(file);
-    }, (delay /= 2));
-  }))
-  .then(readCb));
-
-Promise.all(arrayOfPromises)
-  .then(completedAll)
-  .catch(console.error);
